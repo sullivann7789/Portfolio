2265d80802a7fbce3072f547fddee19b65a3d4f3
Geez-Us <noahsullivan382@gmail.com>
2022-12-01 22:18:31 -0800

    JS edits cd .. pwd

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+var HRSN = Image.querySelector("HRSN")
+var PWSG = Image.querySelector("PWSG")
+var PWSG2 = Image.querySelector("PWSG2")
+
+HRSN.onclick = window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
+PWSG.onclick = window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
+PWSG2.onclick = window.open("https://sullivann7789.github.io/Horiseon-Refactor-Project/", "_blank");
+
+function opensesame () {
+
+}
