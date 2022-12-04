567ffdf15e397fa3946edd0739cf284d6b28fb34
Geez-Us <noahsullivan382@gmail.com>
2022-12-03 20:53:07 -0800

    edits

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,26 @@
-const hrsn = window.open("https://sullivann7789.github.io/Horiseon-Refactor-Project", "_blank");
-const pwsg = window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
+
 
 let bang = document.querySelector("img");
 const src = bang.getAttribute("src");
 
 bang.onclick = () => {
     if (src === "HRSN.png") {
-        hrsn
+        const hrsn = window.open("https://sullivann7789.github.io/Horiseon-Refactor-Project", "_blank");
     } else {
-        pwsg
+        const pwsg = window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
+    }
+    if (!src === "HRSN.png") {
+        const pwsg =  window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
+    } else {
+        const hrsn = window.open("https://sullivann7789.github.io/Horiseon-Refactor-Project", "_blank");
+    }
+    };
+
+let stang = document.querySelectorAll("img");
+const hrc = bang.getAttribute("src");
+
+stang.onclick = () => {
+    if (hrc === "PWSG.png") {
+        const dork = window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
     }
 };
