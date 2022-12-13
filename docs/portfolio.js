7445da97d923ca565d80a0dedab3c0cc83a66e3b
Geez-Us <noahsullivan382@gmail.com>
2022-12-12 16:47:50 -0800

    design layout

---
+++ docs/portfolio.js
@@ -0,0 +1,15 @@
+const HRSN = Image("HRSN.png");
+const PWSG = Image("PWSG.png");
+const PWSG2 = Image("PWSG2.png");
+
+function A () {
+    HRSN.onclick = window.open("https://sullivann7789.github.io/Horiseon-Refactor-Project/", "_blank");
+};
+
+function B () {
+    PWSG.onclikc = window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
+};
+
+function C () {
+    PWSG2.onclick = window.open("https://sullivann7789.github.io/prework-study-guide/", "_blank");
+};
